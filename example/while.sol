6f26a1ed5e5d8fa8a0ae1f2e2b6d66325cc07020
Koukyosyumei <koukyosyumei@hotmail.com>
2024-07-31 19:03:39 +0900

    Update

--- example/while.sol
+++ example/while.sol
@@ -3,15 +3,6 @@ pragma solidity ^0.8.0;
 
 contract WhileContract {
     function check(uint32 n) public pure {
-<<<<<<< HEAD
-        uint32 x = n;
-        uint32 s = 0;
-        while (x > 0) {
-            s += x;
-            x -= 1;
-        }
-        assert(s == n * (n + 1) / 2);
-=======
         if (n < 10) {
             uint32 x = n;
             uint32 s = 0;
@@ -24,6 +15,5 @@ contract WhileContract {
             }
             assert(s == n * (n + 1) / 2);
         }
->>>>>>> bfb662d2009f9ba5cd39798a96ed6a2c297a9b58
     }
 }
