f354578733e9230840469cd1ce7fad6b83537ae1
Koukyosyumei <koukyosyumei@hotmail.com>
2024-08-09 19:42:47 -0400

    add examples

---
+++ example/callwithinput.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CalleeWithInput {
+    function check(uint x) public pure {
+        assert(x + 10 == 100);
+    }
+}
+
+contract CallerWithInput {
+    CalleeWithInput callee;
+
+    function setUp() public {
+        callee = new CalleeWithInput();
+    }
+
+    function callcheck(uint y) public view {
+        callee.check(y);
+    }
+}
+
