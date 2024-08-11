678e3ce2fbaca45c164a8fba4f05a3537173378d
Koukyosyumei <koukyosyumei@hotmail.com>
2024-08-11 08:53:28 -0400

    update

--- example/storeload.sol
+++ example/storeload.sol
@@ -3,14 +3,16 @@ pragma solidity ^0.8.0;
 
 contract StoreLoad {
     uint s;
+    uint t;
 
-    function store(uint x) public {
+    function store(uint x, uint y) public {
         s = x;
+        t = y;
     }
 
-    function load(uint y) public view {
-        if (s > 1 && y > 1 && s < 100 && y < 100) {
-            assert(s * y != 12);
+    function load(uint z) public view {
+        if (s > 1 && t > 1 &&  z > 1 && s < 100 && t < 100 && z < 100) {
+            assert(s * t * z != 30);
         }
     }
 }
